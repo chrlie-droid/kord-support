2fa64845d1b56ec7d7c1bd05baa0ab9310baf0a7
chrlie-droid <aliferovdg@gmail.com>
2026-06-23 11:20:35 +0300

    Backend: include tickets API router

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from backend.app.api.crm import router as crm_router
 from backend.app.api.health import router as health_router
 from backend.app.api.passport import router as passport_router
+from backend.app.api.tickets import router as tickets_router
 
 api_router = APIRouter()
 api_router.include_router(health_router)
 api_router.include_router(crm_router)
 api_router.include_router(passport_router)
+api_router.include_router(tickets_router)
